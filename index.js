6c17f26d37c55df83815677bf27c245fb4ed2598
machmaegal <aka.pleat371@passinbox.com>
2024-11-11 19:20:28 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.items.length - 1) throw new Error("OutOfBounds");
+    return this.items.at(pos);
+  }
 
-  max() {}
+  max() {
+    if (!this.items.length) throw new Error("EmptySortedList");
+    return this.items.at(-1);
+  }
 
-  min() {}
+  min() {
+    if (!this.items.length) throw new Error("EmptySortedList");
+    return this.items.at(0);
+  }
 
-  sum() {}
+  sum() {
+    if (!this.items.length) return 0;
+    return this.items.reduce((acc, item) => acc + item);
+  }
 
-  avg() {}
+  avg() {
+    if (!this.items.length) throw new Error("EmptySortedList");
+    return this.sum() / this.items.length;
+  }
 }
 
 module.exports = SortedList;
